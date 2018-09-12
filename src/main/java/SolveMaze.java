b7233a160c1d4f1e04b2adca872086eff1c5c0bd
chanmin <chanmin2@illinois.edu>
2018-09-12 14:34:32 -0500

    finished

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,17 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                }
+                maze.move();
+                maze.turnLeft();
+                if (!maze.canMove()) {
+                    maze.turnRight();
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
